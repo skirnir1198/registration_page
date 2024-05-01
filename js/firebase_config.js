150da1bbd7d8e34de664286402512a38ad99daa6
miyazakiari <skirnir11981198@gmail.com>
2024-05-01 21:08:31 +0900

    analyticsコード削除

--- js/firebase_config.js
+++ js/firebase_config.js
@@ -9,13 +9,5 @@
     appId: "1:1043757292649:web:a65e564cf2f157194996f8",
     measurementId: "G-DL6QKE1FJC"
   });
-  const db = firebase.firestore();
-  const disableAnalytics = localStorage.getItem('disableAnalytics'); //開発者はtrue;
-if (window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1" && !disableAnalytics) {
-  firebase.analytics();
-} else {
-  console.log("The website is running on localhost && hostUser.");
-}
-
   firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 }
